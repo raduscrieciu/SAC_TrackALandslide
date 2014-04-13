e3bd2a146b2c156d3078e71cf5eeb5a172cfbcb8
raduscrieciu <radu.scrieciu@gmail.com>
2014-04-13 13:57:59 +0100

    minor improvements

--- TrackALandslide/src/com/example/trackalandslide/Utils.java
+++ TrackALandslide/src/com/example/trackalandslide/Utils.java
@@ -22,11 +22,14 @@ public class Utils {
 	
 	private MainActivity context;
 	
+	private int riskType=0;
+	
 	public Utils(Context context){
 		this.context=(MainActivity) context;
 	}
 	
 	public double computeResult(){
+		
 		if(standardDeviation<5){
 			deviationRisk=RESULT_LOW;
 		}else if(standardDeviation>=5 && standardDeviation<10){
@@ -62,18 +65,72 @@ public class Utils {
 		
 		RelativeLayout resultLayout=(RelativeLayout) context.findViewById(R.id.resultLayout);
 		TextView resultText=(TextView) context.findViewById(R.id.finalResultText);
+		TextView typeText=(TextView) context.findViewById(R.id.typeText);
 		
-		if(resultCode<=1){
-			resultText.setText("Low Risk");
-			resultLayout.setBackgroundResource(R.drawable.green_result);
-		}else if(resultCode<2 && resultCode>1){
-			resultText.setText("Medium Risk");
-			resultLayout.setBackgroundResource(R.drawable.yellow_result);
+		double flow=(rainfallRisk+deviationRisk)/2;
+		
+		if(flow<=1){
+//			resultText.setText("Flow: Low Risk");
+//			resultLayout.setBackgroundResource(R.drawable.green_result);
+//			typeText.setText("Flow");
+		}else if(flow<2 && resultCode>1){
+//			resultText.setText("Flow: Medium Risk");
+//			resultLayout.setBackgroundResource(R.drawable.yellow_result);
+//			typeText.setText("Flow");
+			riskType=1;
 		}
-		else if(resultCode>=2){
+		else if(flow>=2){
+			
+			riskType=1;
+		}
+		
+		if(standardDeviation<=20){
+//			resultText.setText("Fall: Low Risk");
+//			resultLayout.setBackgroundResource(R.drawable.green_result);
+		}else if(resultCode>20 && resultCode<30){
+		
+			riskType=2;
+		}
+		else if(resultCode>=30){
+//			resultText.setText("Fall: High Risk");
+//			resultLayout.setBackgroundResource(R.drawable.red_result);
+			riskType=2;
+		}
+		
+		if(standardDeviation<=2){
+//			resultText.setText("Topple: Low Risk");
+//			resultLayout.setBackgroundResource(R.drawable.green_result);
+		}else{
+//			resultText.setText("Topple: Low Risk");
+//			resultLayout.setBackgroundResource(R.drawable.green_result);
+//			
+			if(earthquakeRisk>1){
+//				resultText.setText("Topple: High Risk");
+//				resultLayout.setBackgroundResource(R.drawable.red_result);
+				riskType=3;
+			}
+		}
+		
+		if(riskType==1){
 			resultText.setText("High Risk");
 			resultLayout.setBackgroundResource(R.drawable.red_result);	
+			typeText.setText("Flow");
+		}else if(riskType==2){
+			resultText.setText("Medium Risk");
+			resultLayout.setBackgroundResource(R.drawable.yellow_result);
+			typeText.setText("Fall");
+			
 		}
+		else if(riskType==3){
+			resultText.setText("High Risk");
+			resultLayout.setBackgroundResource(R.drawable.red_result);
+			typeText.setText("Topple");
+		}else{
+			resultText.setText("Low Risk");
+			resultLayout.setBackgroundResource(R.drawable.green_result);	
+			typeText.setText("");
+		}
+		
 		
 		AlphaAnimation anim = new AlphaAnimation(0, 1);
 		anim.setDuration(2500);
